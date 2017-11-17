f37ce9ef0e0c50f0c19f6dd30caccbbefade3c13
Bianshilong <442668830@qq.com>
2017-11-17 17:59:42 +0800

    add for spring

--- Spring43-Aop/src/dao/impl/UserDaoImpl.java
+++ Spring43-Aop/src/dao/impl/UserDaoImpl.java
@@ -14,6 +14,7 @@ public class UserDaoImpl implements UserDao{
 	public void save(User user) {
 		//这里并没有实现完整的数据库操作，仅为了说明问题
 		System.out.println("保存用户信息到数据库！");
+		System.out.println(user);
 	}
 	
 	
